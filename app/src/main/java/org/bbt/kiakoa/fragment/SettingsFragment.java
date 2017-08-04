d8991142cdb013ec9956ab11f658dffa51cf389f
Benoît <benoit.development@gmail.com>
2017-08-04 16:10:02 +0200

    Manage READ_CONTACTS permission in LendDetails

--- app/src/main/java/org/bbt/kiakoa/fragment/SettingsFragment.java
+++ app/src/main/java/org/bbt/kiakoa/fragment/SettingsFragment.java
@@ -1,8 +1,20 @@
 package org.bbt.kiakoa.fragment;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import org.bbt.kiakoa.R;
 
@@ -10,6 +22,17 @@ import org.bbt.kiakoa.R;
  * Fragment managing settings
  */
 public class SettingsFragment extends PreferenceFragment {
+
+
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 0;
+    /**
+     * For Log
+     */
+    private static final String TAG = "SettingsFragment";
+    /**
+     * {@link SwitchPreference} managing read contact permission
+     */
+    private SwitchPreference contactReadPref;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
