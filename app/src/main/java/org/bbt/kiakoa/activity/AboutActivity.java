11953ea2eaba0d0b702a999dd05e370cae125d25
Benoît <benoit.development@gmail.com>
2017-09-29 16:39:56 +0200

    More information in about page

--- app/src/main/java/org/bbt/kiakoa/activity/AboutActivity.java
+++ app/src/main/java/org/bbt/kiakoa/activity/AboutActivity.java
@@ -1,5 +1,6 @@
 package org.bbt.kiakoa.activity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -69,5 +70,42 @@ public class AboutActivity extends AppCompatActivity {
             }
         });
 
+        // rate application
+        findViewById(R.id.rate_app).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("market://details?id=" + getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                // To count with Play market back stack, After pressing back button,
+                // to taken back to our application, we need to add following flags to intent.
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+                }
+            }
+        });
+
+        // privacy policy
+        findViewById(R.id.privacy).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://blogdebenoit.wordpress.com/my-loan-lists-privacy-policy/"));
+                startActivity(i);
+            }
+        });
+
+        // readme
+        findViewById(R.id.readme).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/benoit-development/KiaKoa/blob/master/README.md"));
+                startActivity(i);
+            }
+        });
     }
 }
